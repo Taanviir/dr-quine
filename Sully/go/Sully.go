8c076b9f5ddec40a1362c49b1fe970cc99500bfa
Taanviir <66136914+Taanviir@users.noreply.github.com>
2025-12-27 00:17:30 +0400

    feat: add Sully quine program in Go with Makefile and module definition

---
+++ Sully/go/Sully.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	i := 5
+	if i < 0 {	return }
+
+	filename := fmt.Sprintf("Sully_%d.go", i)
+	file, err := os.Create(filename)
+	if err != nil { panic(err) }
+	defer file.Close()
+
+	template := "package main\n\nimport (\n\t\"fmt\"\n\t\"os\"\n\t\"os/exec\"\n)\n\nfunc main() {\n\ti := %d\n\tif i < 0 {	return }\n\n\tfilename := fmt.Sprintf(\"Sully_%%d.go\", i)\n\tfile, err := os.Create(filename)\n\tif err != nil { panic(err) }\n\tdefer file.Close()\n\n\ttemplate := %q\n\tfmt.Fprintf(file, template, i-1, template)\n\n\tcompile := fmt.Sprintf(\"go build -o Sully_%%d Sully_%%d.go\", i, i)\n\texec.Command(\"bash\", \"-c\", compile).Run()\n\n\texecute := fmt.Sprintf(\"./Sully_%%d\", i)\n\texec.Command(\"bash\", \"-c\", execute).Run()\n}\n"
+	fmt.Fprintf(file, template, i-1, template)
+
+	compile := fmt.Sprintf("go build -o Sully_%d Sully_%d.go", i, i)
+	exec.Command("bash", "-c", compile).Run()
+
+	execute := fmt.Sprintf("./Sully_%d", i)
+	exec.Command("bash", "-c", execute).Run()
+}
---
+++ Sully/go/go.mod
@@ -0,0 +1,3 @@
+module sully
+
+go 1.23.2
