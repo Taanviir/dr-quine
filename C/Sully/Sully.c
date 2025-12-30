a3a5f677b92661a89ffd6e6b7ba918f304a7c37d
Taanviir <66136914+Taanviir@users.noreply.github.com>
2025-12-30 21:01:44 +0400

    fix: Sully (C|GO) now correctly prints files 4-->0

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -3,22 +3,22 @@
 
 int main() {
   int i = 5;
-  if (i < 0) return 0;
+  if (i == 0) return 0;
 
   char filename[20], compile[50], execute[30];
-  snprintf(filename, sizeof(filename), "Sully_%d.c", i);
+  snprintf(filename, sizeof(filename), "Sully_%d.c", i-1);
 
   FILE *f = fopen(filename, "w");
   if (!f) return 1;
 
-  char *src = "#include <stdio.h>%c#include <stdlib.h>%c%cint main() {%c  int i = %d;%c  if (i < 0) return 0;%c%c  char filename[20], compile[50], execute[30];%c  snprintf(filename, sizeof(filename), %cSully_%%d.c%c, i);%c%c  FILE *f = fopen(filename, %cw%c);%c  if (!f) return 1;%c%c  char *src = %c%s%c;%c  fprintf(f,src,10,10,10,10,i-1,10,10,10,10,34,34,10,10,34,34,10,10,10,34,src,34,10,10,10,10,34,34,10,10,10,34,34,10,10,10,10);%c  fclose(f);%c%c  snprintf(compile, sizeof(compile), %cgcc -Wall -Wextra -Werror -o Sully_%%d Sully_%%d.c%c, i, i);%c  system(compile);%c%c  snprintf(execute, sizeof(execute), %c./Sully_%%d%c, i);%c  system(execute);%c  return 0;%c}%c";
+  char *src = "#include <stdio.h>%c#include <stdlib.h>%c%cint main() {%c  int i = %d;%c  if (i == 0) return 0;%c%c  char filename[20], compile[50], execute[30];%c  snprintf(filename, sizeof(filename), %cSully_%%d.c%c, i-1);%c%c  FILE *f = fopen(filename, %cw%c);%c  if (!f) return 1;%c%c  char *src = %c%s%c;%c  fprintf(f,src,10,10,10,10,i-1,10,10,10,10,34,34,10,10,34,34,10,10,10,34,src,34,10,10,10,10,34,34,10,10,10,34,34,10,10,10,10);%c  fclose(f);%c%c  snprintf(compile, sizeof(compile), %cgcc -Wall -Wextra -Werror -o Sully_%%d Sully_%%d.c%c, i-1, i-1);%c  system(compile);%c%c  snprintf(execute, sizeof(execute), %c./Sully_%%d%c, i-1);%c  system(execute);%c  return 0;%c}%c";
   fprintf(f,src,10,10,10,10,i-1,10,10,10,10,34,34,10,10,34,34,10,10,10,34,src,34,10,10,10,10,34,34,10,10,10,34,34,10,10,10,10);
   fclose(f);
 
-  snprintf(compile, sizeof(compile), "gcc -Wall -Wextra -Werror -o Sully_%d Sully_%d.c", i, i);
+  snprintf(compile, sizeof(compile), "gcc -Wall -Wextra -Werror -o Sully_%d Sully_%d.c", i-1, i-1);
   system(compile);
 
-  snprintf(execute, sizeof(execute), "./Sully_%d", i);
+  snprintf(execute, sizeof(execute), "./Sully_%d", i-1);
   system(execute);
   return 0;
 }
--- GO/Sully/Sully.go
+++ GO/Sully/Sully.go
@@ -8,19 +8,23 @@ import (
 
 func main() {
 	i := 5
-	if i < 0 {	return }
+	if i == 0 {
+		return
+	}
 
-	filename := fmt.Sprintf("Sully_%d.go", i)
+	filename := fmt.Sprintf("Sully_%d.go", i-1)
 	file, err := os.Create(filename)
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 	defer file.Close()
 
-	template := "package main\n\nimport (\n\t\"fmt\"\n\t\"os\"\n\t\"os/exec\"\n)\n\nfunc main() {\n\ti := %d\n\tif i < 0 {	return }\n\n\tfilename := fmt.Sprintf(\"Sully_%%d.go\", i)\n\tfile, err := os.Create(filename)\n\tif err != nil { panic(err) }\n\tdefer file.Close()\n\n\ttemplate := %q\n\tfmt.Fprintf(file, template, i-1, template)\n\n\tcompile := fmt.Sprintf(\"go build -o Sully_%%d Sully_%%d.go\", i, i)\n\texec.Command(\"bash\", \"-c\", compile).Run()\n\n\texecute := fmt.Sprintf(\"./Sully_%%d\", i)\n\texec.Command(\"bash\", \"-c\", execute).Run()\n}\n"
+	template := "package main\n\nimport (\n\t\"fmt\"\n\t\"os\"\n\t\"os/exec\"\n)\n\nfunc main() {\n\ti := %d\n\tif i == 0 {	return }\n\n\tfilename := fmt.Sprintf(\"Sully_%%d.go\", i-1)\n\tfile, err := os.Create(filename)\n\tif err != nil { panic(err) }\n\tdefer file.Close()\n\n\ttemplate := %q\n\tfmt.Fprintf(file, template, i-1, template)\n\n\tcompile := fmt.Sprintf(\"go build -o Sully_%%d Sully_%%d.go\", i-1, i-1)\n\texec.Command(\"bash\", \"-c\", compile).Run()\n\n\texecute := fmt.Sprintf(\"./Sully_%%d\", i-1)\n\texec.Command(\"bash\", \"-c\", execute).Run()\n}\n"
 	fmt.Fprintf(file, template, i-1, template)
 
-	compile := fmt.Sprintf("go build -o Sully_%d Sully_%d.go", i, i)
+	compile := fmt.Sprintf("go build -o Sully_%d Sully_%d.go", i-1, i-1)
 	exec.Command("bash", "-c", compile).Run()
 
-	execute := fmt.Sprintf("./Sully_%d", i)
+	execute := fmt.Sprintf("./Sully_%d", i-1)
 	exec.Command("bash", "-c", execute).Run()
 }
